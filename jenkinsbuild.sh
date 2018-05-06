26e620b4c82270d7de97d14601a8c6b764dda9e4
karthikmarimuthu <34113109+karthikmarimuthu@users.noreply.github.com>
2018-05-06 21:21:43 +1000

    Update jenkinsbuild.sh

--- jenkinsbuild.sh
+++ jenkinsbuild.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 BUILD_NU=$(echo $BUILD_NUMBER)
 REL=$(for i in $(grep '<version>' pom.xml); do ver=${i%<*}; ver=${ver#*>}; echo "$ver"; done | head -1)
-VERSION=$REL.$BUILD_NU 
-echo "$VERSION" > /tmp/auto/release-$app.txt
+TVER=$REL.$BUILD_NU
+echo "VERSION=$TVER" > /tmp/auto/release-$app.txt
 chmod -R 777 /tmp/auto/release-$app.txt
-source /tmp/auto/release-$app.txt
 if echo "$VERSION" | grep -q "SNAPSHOT"; then
-repo=echo "snapshots"
+echo "repo=snapshots" > /tmp/auto/release-$app.txt
+source /tmp/auto/release-$app.txt
 wdir=/data/nexus/storage/$repo/au/
 /bin/bash /usr/local/sbin/reporemove.sh
 /bin/bash /usr/local/sbin/repoadd.sh
 else
-repo=echo "releases"
+echo "repo=releases" > /tmp/auto/release-$app.txt
+source /tmp/auto/release-$app.txt
 wdir=/data/nexus/storage/$repo/au/
 /bin/bash /usr/local/sbin/reporemove.sh
 /bin/bash /usr/local/sbin/repoadd.sh
