3d8d94964bd00a197b6cdd4adbf53bdba4d29f2f
karthikmarimuthu <34113109+karthikmarimuthu@users.noreply.github.com>
2018-05-07 17:18:58 +1000

    Update jenkinsbuild.sh

--- jenkinsbuild.sh
+++ jenkinsbuild.sh
@@ -2,18 +2,26 @@
 BUILD_NU=$(echo $BUILD_NUMBER)
 REL=$(for i in $(grep '<version>' pom.xml); do ver=${i%<*}; ver=${ver#*>}; echo "$ver"; done | head -1)
 TVER=$REL.$BUILD_NU
-echo "VERSION=$TVER" > /tmp/auto/release-$app.txt
+echo "VERSION=$TVER" >> /tmp/auto/release-$app.txt
 chmod -R 777 /tmp/auto/release-$app.txt
-if echo "$VERSION" | grep -q "SNAPSHOT"; then
-echo "repo=snapshots" > /tmp/auto/release-$app.txt
 source /tmp/auto/release-$app.txt
-wdir=/data/nexus/storage/$repo/au/
-/bin/bash /usr/local/sbin/reporemove.sh
-/bin/bash /usr/local/sbin/repoadd.sh
+echo $VERSION
+if echo "$VERSION" | grep -q "SNAPSHOT" ; then
+	echo "It is a snapshot"
+#echo "repo=snapshots" >> /tmp/auto/release-$app.txt
+	export repo=snapshots
+	export wdir=/data/nexus/storage/$repo/
+	echo "$wdir"
+	echo "$repo"
+	/bin/bash /usr/local/sbin/reporemove.sh
+	/bin/bash /usr/local/sbin/repoadd.sh
 else
-echo "repo=releases" > /tmp/auto/release-$app.txt
-source /tmp/auto/release-$app.txt
-wdir=/data/nexus/storage/$repo/au/
-/bin/bash /usr/local/sbin/reporemove.sh
-/bin/bash /usr/local/sbin/repoadd.sh
+#echo "repo=releases" >> /tmp/auto/release-$app.txt
+	export repo=releases`
+	export wdir=/data/nexus/storage/$repo/`
+#source /tmp/auto/release-$app.txt
+	echo "The wdir is :$wdir"
+	echo "the repo is :$repo"
+	/bin/bash /usr/local/sbin/reporemove.sh
+	/bin/bash /usr/local/sbin/repoadd.sh
 fi
