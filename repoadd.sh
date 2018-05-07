82fd1ba8721f5cc290ba75463c6b0e10c4d08151
karthikmarimuthu <34113109+karthikmarimuthu@users.noreply.github.com>
2018-05-07 17:04:21 +1000

    Update repoadd.sh

--- repoadd.sh
+++ repoadd.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#Ensure add gpg keys to jenkins using export-import
 newf=$(find "$wdir" -type f -printf '%T@ %p\n' | sort -n | grep ".deb$" | cut -f2- -d" " | tail -1)
-newfile=$(echo $newf | sed 's_/.nexus/attributes/_/_g')
+file=$(echo $newf | sed 's_/.nexus/attributes/_/_g')
 #file=$(basename $newfile)
 if [[ "$file" == *.deb ]];
         then
                # repo=$(get_repo $pkg)
-                echo "Adding file: $newfile in repo: $repo"
+                echo "Adding file: $file in repo: $repo"
                 sudo aptly repo add $repo $newfile
                 sudo aptly publish update -batch=true -keyring="/root/.gnupg/pubring.gpg" -secret-keyring="/root/.gnupg/secring.gpg" -gpg-key="3B5F5502" -passphrase="mGvV87Uo3av+5ZuRm3aiAIxo8IWoah" $repo
         else
-                echo "file: "$newfile" is not a valid .deb file, skipping"
+                echo "file: "$file" is not a valid .deb file, skipping"
         fi
