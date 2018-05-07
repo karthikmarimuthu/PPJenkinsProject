fb90a441588340696608c143f47377725a7e6939
karthikmarimuthu <34113109+karthikmarimuthu@users.noreply.github.com>
2018-05-07 15:46:49 +1000

    Update repoadd.sh

--- repoadd.sh
+++ repoadd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-newfile=$(find "$wdir" -type f -printf '%T@ %p\n' | sort -n | grep ".deb$" | cut -f2- -d" " | tail -1)
-file=$(basename $newfile)
+newf=$(find "$wdir" -type f -printf '%T@ %p\n' | sort -n | grep ".deb$" | cut -f2- -d" " | tail -1)
+newfile=$(echo $newf | sed 's_/.nexus/attributes/_/_g')
+#file=$(basename $newfile)
 if [[ "$file" == *.deb ]];
         then
-                repo=$(get_repo $pkg)
-                echo "Adding file: $pkg in repo: $repo"
-                aptly repo add $repo $pkg
-                aptly publish update -batch=true -keyring="/root/.gnupg/pubring.gpg" -secret-keyring="/root/.gnupg/secring.gpg" -gpg-key="3B5F5502" -
-passphrase="mGvV87Uo3av+5ZuRm3aiAIxo8IWoah" $repo
+               # repo=$(get_repo $pkg)
+                echo "Adding file: $newfile in repo: $repo"
+                sudo aptly repo add $repo $newfile
+                sudo aptly publish update -batch=true -keyring="/root/.gnupg/pubring.gpg" -secret-keyring="/root/.gnupg/secring.gpg" -gpg-key="3B5F5502" -passphrase="mGvV87Uo3av+5ZuRm3aiAIxo8IWoah" $repo
         else
-                echo "file: "$pkg" is not a valid .deb file, skipping"
+                echo "file: "$newfile" is not a valid .deb file, skipping"
         fi
